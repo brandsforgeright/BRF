d32ad0d35a365ae9113053577a5d1c4c31cb72a6
brandsforgeright <brandsforgeright@gmail.com>
2026-05-21 14:07:11 +0500

    Add files via upload

---
+++ sites/2dR3gCIkD247chguaATZYW/Fm2dCcGaTqKZcgHLEmIhe9MpXk75rsanHsrpUUjEGuE.DGdL2OvO.mjs
@@ -0,0 +1,20 @@
+import{t as e}from"./rolldown-runtime.uxh0A31G.mjs";async function t(e,t,i){let a=r[e],o=a?await a(t,i):void 0,s={bodyEnd:[],bodyStart:[],headEnd:[],headStart:[]};for(let t of n){if(t.pageIds&&!t.pageIds.has(e))continue;let n=t.code(o);n&&s[t.placement].push({...t,code:n})}return s}var n,r,i,a;e((()=>{n=[{code:e=>`<style> \r
+\r
+#__framer-badge-container  { \r
+\r
+    pointer-events: auto; \r
+\r
+    display: none; \r
+\r
+} \r
+\r
+#__framer-editorbar-container, \r
+\r
+#__framer-editorbar-container * { \r
+\r
+  display: none !important; \r
+\r
+} \r
+\r
+</style> `,id:`hODdnOQvy`,loadMode:`always`,name:`Hide framer badge`,placement:`bodyEnd`}],r={},i={bodyEnd:[`hODdnOQvy`],bodyStart:[],headEnd:[],headStart:[]},a={exports:{snippetsSorting:{type:`variable`,annotations:{framerContractVersion:`1`}},getSnippets:{type:`function`,annotations:{framerContractVersion:`1`}},__FramerMetadata__:{type:`variable`}}}}))();export{a as __FramerMetadata__,t as getSnippets,i as snippetsSorting};
+//# sourceMappingURL=Fm2dCcGaTqKZcgHLEmIhe9MpXk75rsanHsrpUUjEGuE.DGdL2OvO.mjs.map
